7789164fa6b1f65e8fdb44342e02d526a8e813aa
PerePerez <pereperezlopez@hotmail.com>
2022-10-30 01:07:16 +0200

    Esborrat buscador de la pagina incial. Ara només apareixen dispositus amb el UUID 19B10000E8F2537E4F6CD104768A1214 de Servei

--- App Mobil/flutter_reactive_ble-master/example/lib/src/ui/device_list.dart
+++ App Mobil/flutter_reactive_ble-master/example/lib/src/ui/device_list.dart
@@ -69,8 +69,7 @@ class _DeviceListState extends State<_DeviceList> {
   }
 
   void _startScanning() {
-    final text = _uuidController.text;
-    widget.startScan(text.isEmpty ? [] : [Uuid.parse(_uuidController.text)]);
+    widget.startScan([]);
   }
 
   @override
@@ -85,18 +84,6 @@ class _DeviceListState extends State<_DeviceList> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const SizedBox(height: 16),
-                  const Text('Service UUID (2, 4, 16 bytes):'),
-                  TextField(
-                    controller: _uuidController,
-                    enabled: !widget.scannerState.scanIsInProgress,
-                    decoration: InputDecoration(
-                        errorText:
-                            _uuidController.text.isEmpty || _isValidUuidInput()
-                                ? null
-                                : 'Invalid UUID format'),
-                    autocorrect: false,
-                  ),
                   const SizedBox(height: 16),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -122,7 +109,7 @@ class _DeviceListState extends State<_DeviceList> {
                     children: [
                       Expanded(
                         child: Text(!widget.scannerState.scanIsInProgress
-                            ? 'Enter a UUID above and tap start to begin scanning'
+                            ? 'Tap start to begin scanning'
                             : 'Tap a device to connect to it'),
                       ),
                       if (widget.scannerState.scanIsInProgress ||
